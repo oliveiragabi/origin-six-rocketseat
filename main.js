14293f83ded3c04dcc9819358c46bce45d91b8db
Gabrielle Oliveira de Souza <gabrielle.souza@ibm.com>
2022-05-24 15:43:02 -0300

    criando footer e estilizando, ajustando scroll

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -28,4 +28,48 @@ window.addEventListener('scroll', function () {
     // menor que a altura do header
     header.classList.remove('scroll')
   }
+}
+
+/* SLIDE WITH SWIPER */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/*SCROLL REVIEW*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+#about .image #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links,
+footer .brand, footer .social
+`,
+  { interval: 100 }
+)
+
+/*BACK TO TOP*/
+function backToTop() {
+  const buttonBackToTop = document.querySelector('.back-to-top')
+  if (this.window.scrollY >= 560) {
+    buttonBackToTop.classList.add('show')
+  } else {
+    buttonBackToTop.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
 })
